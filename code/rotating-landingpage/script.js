aef4e3d5be7b8b251de66c43b0743aad84943127
GreyBeardDoer <ips.qsf08@gmail.com>
2023-02-05 17:30:28 +0530

    Project done

---
+++ code/rotating-landingpage/script.js
@@ -0,0 +1,17 @@
+const getContainer = document.querySelector('.container');
+const getCircle = document.querySelector('.circle');
+const showMenu = document.getElementById('menu-open');
+const hideMenu = document.getElementById('menu-close');
+
+showMenu.addEventListener('click', openMenu);
+closeMenu.addEventListener('click', closeMenu);
+
+function openMenu() {
+    getContainer.classList.add('menu-active');
+    getCircle.classList.add('menu-active');
+}
+
+function closeMenu() {
+    getContainer.classList.remove('menu-active');
+    getCircle.classList.remove('menu-active');
+}
