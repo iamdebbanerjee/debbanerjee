3b52cd227822694b770a6f25d335f79ad6b42afa
Dev <26302604+debu2code@users.noreply.github.com>
2023-02-24 15:50:48 +0530

    Saving values in local storage tested

--- code/income-tax-app/script.js
+++ code/income-tax-app/script.js
@@ -1,13 +1,27 @@
 // Global variables declarations
 
-const okBtn = document.querySelectorAll('.save-and-move-ahead');
+const okBtn = document.querySelectorAll('.save');
+let userBasic = document.getElementById('basic-salary');
+let userDA = document.getElementById('da-rate');
+let userHRA = document.getElementById('hra-rate');
+let userTPA = document.getElementById('tpa-rate');
 
-
-
+// okBtn[1].addEventListener('click', () => {console.log(userBasic.value), console.log(userDA.value)});
 
 
 // Button events
 
+okBtn.forEach((save) => {
+    save.addEventListener('click', saveLocalStorage);
+})
 
 
+// Functions
+
+function saveLocalStorage() {
+    localStorage.setItem('Basic', JSON.stringify(userBasic.value));
+    localStorage.setItem('DA', JSON.stringify(userDA.value));
+    localStorage.setItem('HRA', JSON.stringify(userHRA.value));
+    localStorage.setItem('TPA', JSON.stringify(userTPA.value));
+}
 
