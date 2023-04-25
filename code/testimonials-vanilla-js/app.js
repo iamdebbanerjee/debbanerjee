a35059efd433aa2ffd3421bb3f0bf971ae578be0
Dev <26302604+GreyBeardDoer@users.noreply.github.com>
2023-04-25 16:56:53 +0530

    Reduced testimonial text length

--- code/testimonials-vanilla-js/app.js
+++ code/testimonials-vanilla-js/app.js
@@ -5,28 +5,28 @@ const reviews = [
     name: 'susan smith',
     job: 'web developer',
     img: 'https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg',
-    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring.",
   },
   {
     id: 2,
     name: 'anna johnson',
     job: 'web designer',
     img: 'https://images2.imgbox.com/2e/6e/JAMvTZ56_o.jpeg',
-    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee.',
   },
   {
     id: 3,
     name: 'peter jones',
     job: 'intern',
     img: 'https://images2.imgbox.com/56/88/oJvFN3l5_o.jpeg',
-    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb.',
   },
   {
     id: 4,
     name: 'bill anderson',
     job: 'the boss',
     img: 'https://images2.imgbox.com/8b/1c/vwWNTsCd_o.jpeg',
-    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz.',
   },
 ];
 
