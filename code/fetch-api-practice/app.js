b7a4059e09ae30ea67034949686d05f3874af11f
Quantophilic <ips.qsf08@gmail.com>
2023-05-06 13:09:08 +0530

    Fetch API call with JSON Placeholder API

---
+++ code/fetch-api-practice/app.js
@@ -0,0 +1,20 @@
+function createPost(post) {
+	fetch('https://jsonplaceholder.typicode.com/posts', {
+		method: 'POST',
+		body: JSON.stringify({ title, content } = post),
+		headers: {
+			'Content-Type': 'application/json',
+			token: 'token987654321'
+		} 
+	})
+	.then((res) => {
+		return res.json();
+	})
+	.then((data) => {
+		console.log(data);
+	})
+	// .catch(console.log("something went wrong"));
+}
+
+
+createPost({title: "Post One", content: "This is content of post one."});
