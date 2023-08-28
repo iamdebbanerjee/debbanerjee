64ad32f15733daf0a2ddab8c80f42edc9f5a67b9
Dev <ips.qsf08@gmail.com>
2023-08-28 06:47:49 +0530

    Todo list display part started

--- code/typicode-todos/app.js
+++ code/typicode-todos/app.js
@@ -1,12 +1,23 @@
 const fetchTodos = async () => {
-    const url = 'https://jsonplaceholder.typicode.com/todos?_limit=10';
+    const url = 'https://jsonplaceholder.typicode.com/todos';
     try {
-        const response = await fetch (url);
+        const response = await fetch (url + "?_limit=10");
         const data = await response.json();
         console.log(data);
+        displayTodos(data);
     } catch (error) {
         console.log(error);
     }
+}
+
+function displayTodos(todos) {
+    const todoDisplay = document.getElementById('todo-display');
+    const todoList = document.createElement('div');
+    todoDisplay.appendChild(todoList);
+    todos.forEach(todo => {
+        const li = document.createElement('li');
+        todoList.appendChild(li);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', fetchTodos);
